dce2de14765b95195b39bf78337d11c6196fb06b
Yash Karandikar <nerdstep710@gmail.com>
2021-10-19 17:10:01 -0500

    Add basic Autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonOne.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous(name="Autonomous 0.00000000000000000000000001 WORKING????")
+public class AutonOne extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        API.init(this);
+        API.print("Status", "Initializing, please wait");
+        MovementAPI.init(API.Motor.M0, API.Motor.M1, API.Motor.M3, API.Motor.M2);
+
+        API.clear();
+        API.print("Press play to start");
+
+        waitForStart();
+
+        MovementAPI.move(0, 1, true);
+        API.pause(1);
+        for (API.Motor m : API.Motor.values()) m.stop();
+    }
+}
