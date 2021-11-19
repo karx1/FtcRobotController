02528c6d94e34fd655f030d9bba03358156644da
Yash Karandikar <nerdstep710@gmail.com>
2021-11-19 16:39:56 -0600

    Add the rest of the servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/API.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/API.java
@@ -170,7 +170,7 @@ public class API {
     }
 
     public enum Servo {
-        S0("s0"), S1("s1"), S2("s2"), S3("s3"), S4("s4"), S5("s5"), S6("s6"), S11("s11");
+        S0("s0"), S1("s1"), S2("s2"), S3("s3"), S4("s4"), S5("s5"), S6("s6"), S7("s7"), S8("s8"), S9("s9"), S10("s10"), S11("s11");
         private final String name;
         private com.qualcomm.robotcore.hardware.Servo servo;
         private Direction direction = Direction.FORWARD;
